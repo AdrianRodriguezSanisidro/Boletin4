323242c6611d19f5e963cf2e51d02bc4446cb8ac
Adry <Adry@Sanisidro>
2016-10-18 10:05:46 +0200

    Inicio

---
+++ Boletin4_2/src/boletin4_2/Boletin4_2.java
@@ -0,0 +1,12 @@
+package boletin4_2;
+/**
+ *
+ * @author Adry
+ */
+public class Boletin4_2 {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ Boletin4_2/src/boletin4_2/Satelite.java
@@ -0,0 +1,13 @@
+package boletin4_2;
+
+/**
+ *
+ * @author Adry
+ */
+public class Satelite {
+    private double meridiano,paralelo,distanciaTerra;
+    public Satelite(){
+        
+    }
+    
+}
