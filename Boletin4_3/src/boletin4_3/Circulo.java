a67a600bd32ff72d29f3ed0aa2c1d8f947c81d99
Adry <Adry@Sanisidro>
2016-10-19 10:20:21 +0200

    Con fallos

---
+++ Boletin4_3/src/boletin4_3/Circulo.java
@@ -0,0 +1,15 @@
+package boletin4_3;
+/**
+ *
+ * @author Adry
+ */
+public class Circulo {
+    private double radio;
+    private final double Pi=3.14;
+public Circulo(){
+    radio=0;
+}
+public Ciculo(){
+    
+}
+}
