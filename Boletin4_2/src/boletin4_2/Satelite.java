49d1685ec57d7c83fd3a251b5e5847d4649c606e
Adry <Adry@Sanisidro>
2016-10-19 09:02:21 +0200

    Fin

--- Boletin4_2/src/boletin4_2/Satelite.java
+++ Boletin4_2/src/boletin4_2/Satelite.java
@@ -7,7 +7,16 @@ package boletin4_2;
 public class Satelite {
     private double meridiano,paralelo,distanciaTerra;
     public Satelite(){
-        
+        meridiano=0;
+        paralelo=0;
+        distanciaTerra=0;
     }
-    
+    public Satelite(double m,double p,double d){
+        meridiano=m;
+        paralelo=p;
+        distanciaTerra=d;
+    }
+    public void verPosicion(){
+        System.out.println("O satelite atopase no meridiano "+meridiano +" paralelo "+ paralelo +"a unha distancia da Terra de "+distanciaTerra);
+}
 }
