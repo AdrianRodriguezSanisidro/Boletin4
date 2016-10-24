626c2a685cd4cfb52c48bc8dac87250ab29a4843
Adry <Adry@Sanisidro>
2016-10-24 22:35:57 +0200

    B4_3 fin

--- Boletin4_3/src/boletin4_3/Boletin4_3.java
+++ Boletin4_3/src/boletin4_3/Boletin4_3.java
@@ -6,7 +6,10 @@ package boletin4_3;
 public class Boletin4_3 {
 
     public static void main(String[] args) {
-        
+        Circulo obxCirculo=new Circulo();
+        obxCirculo.getRadio();
+        obxCirculo.calcularArea();
+        obxCirculo.calcularLonxitude();
     }
     
 }
--- Boletin4_3/src/boletin4_3/Circulo.java
+++ Boletin4_3/src/boletin4_3/Circulo.java
@@ -1,4 +1,7 @@
 package boletin4_3;
+
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Adry
@@ -7,9 +10,27 @@ public class Circulo {
     private double radio;
     private final double Pi=3.14;
 public Circulo(){
-    radio=0;
 }
-public Ciculo(){
+public Circulo(double radio) {
+    this.radio =radio;
+ }
+    public double getRadio() {
+        radio=Float.parseFloat(JOptionPane.showInputDialog("Radio "));
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
     
-}
+    public double calcularArea(){
+        double area=Math.pow(radio, 2f);
+        System.out.println("Area ="+area);
+      return area;
+    }
+    public double calcularLonxitude(){
+        double lonxitude=Pi*radio*2f;
+        System.out.println("Lonxitude ="+ lonxitude);
+        return lonxitude;
+    }
 }
