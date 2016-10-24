2642f15aeea948ceb4d2dfae1b83a6d6507d1042
Adry <Adry@Sanisidro>
2016-10-24 22:34:00 +0200

    JOptions por separado

--- Boletin 4_1/src/boletin/pkg4/Boletin4_1.java
+++ Boletin 4_1/src/boletin/pkg4/Boletin4_1.java
@@ -11,7 +11,8 @@ public class Boletin4_1 {
   public static void main(String[] args) {
         Coche coche1 = new Coche();
         int velocidade;
-        coche1.acelerar(100);
+        
+        coche1.acelerar(Integer.parseInt(JOptionPane.showInputDialog("Intruducir velocidad ")));
         coche1.acelerar(20);
         coche1.frenar(60);
         
